36d5803c2bfc8d05daf50912a69d224663a17260
Mario Posada <marioposadamiranda@gmail.com>
2022-09-20 01:00:37 -0300

    Add fetch

--- src/screens/MarvelScreen.js
+++ src/screens/MarvelScreen.js
@@ -1,17 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import React from 'react'
+import { helpHttp } from "../utils/helpers/helpHttp";
 
-const MarvelScreen =() => {
-    return (
-        <View>
-            <Text>Marvel Screen</Text>
-        </View>
-    )
-}
+const MarvelScreen = () => {
+  const [db, setDb] = useState([]);
 
-const  style= StyleSheet.create({})
+  const options = {
+    params: {
+      apikey: "b851f19c35b735d46106947c516cc797",
+      ts: "1000",
+      hash: "658dde78390b639c423a0952c71709bd",
+    },
+    Headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  let api = helpHttp();
+  let url = `http://gateway.marvel.com/v1/public/comics?ts=${options.params.ts}&apikey=${options.params.apikey}&hash=${options.params.hash}`;
+
+  useEffect(() => {
+    api.get(url).then((res) => {
+      console.warn(res);
+    });
+  }, []);
+
+  return (
+    <View>
+      <Text>Marvel Screen</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({});
 
 export default MarvelScreen;
-
-
