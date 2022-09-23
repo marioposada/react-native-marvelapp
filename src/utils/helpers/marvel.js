11206606b397c1a5fbfe31644b2a3d5100b93a4a
Mario Posada <marioposadamiranda@gmail.com>
2022-09-23 08:44:28 -0300

    Share now

--- src/utils/helpers/marvel.js
+++ src/utils/helpers/marvel.js
@@ -1,8 +1,8 @@
 import md5 from "md5";
 
 const ts = Date.now();
-const privateKey='9812ffff3b8aa2c8302b4b3eeccae8670114693c'
-const publicKey= 'b851f19c35b735d46106947c516cc797'
+const privateKey='YOUR_PRIVATE_KEY'
+const publicKey= 'YOUR_PUBLIC_KEY'
 const hash = md5(`${ts}${privateKey}${publicKey}`);
 const apiParams = {
   ts,
